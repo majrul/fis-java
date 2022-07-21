27717f9377cd1bf113d02e58c583ea0081773e43
mj <mj@192.168.0.124>
2022-07-21 12:36:04 +0530

    java

--- IDontKnowJava/src/com/fis/oop/collection/Example1.java
+++ IDontKnowJava/src/com/fis/oop/collection/Example1.java
@@ -1,12 +1,13 @@
 package com.fis.oop.collection;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Example1 {
 
 	public static void main(String[] args) {
-		List<String> list = new ArrayList<>(5);
+		List<String> list = new ArrayList<>();
 		//<> angular bracket notation in the above code is called as Generics in Java
 		list.add("Java");
 		list.add("Oracle");
@@ -14,15 +15,37 @@ public class Example1 {
 		list.add("JavaScript");
 		list.add("Java");
 		list.add("C#");
+		list.add(2, "C/C++");	
+		list.set(1, "GoLang");
+		
+		List<String> list2 = new ArrayList<>();
+		list2.add("TypeScript");
+		list2.add("Scala");
+		list2.add("Ruby");
+		
+		list.addAll(list2);
 		
 		//System.out.println(list.size());
 		//System.out.println(list.get(2));
 		
+		System.out.println("---- using traditional for loop ----");
 		for(int i=0; i<list.size(); i++) {
 			String str = list.get(i);
 			System.out.println(str);
 		}
+		System.out.println("---- using enhanced for loop ----");
+		for(String str : list)
+			System.out.println(str);
 		
+		System.out.println("---- using lambda for each ----");
+		list.forEach(str -> System.out.println(str));
+		
+		System.out.println("---- using traditional Iterator API ----");
+		Iterator<String> itr = list.iterator();
+		while(itr.hasNext()) {
+			String str = itr.next();
+			System.out.println(str);
+		}
 		//Integer i = 10; //same like String, no need to use new keyword
 		List<Integer> marks = new ArrayList<>();
 		marks.add(15);
---
+++ IDontKnowJava/src/com/fis/oop/collection/Example2.java
@@ -0,0 +1,21 @@
+package com.fis.oop.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fis.oop.api.Person;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		List<Person> list = new ArrayList<>();
+		Person p1 = new Person("Mayur", 22);
+		Person p2 = new Person("Manasa", 22);
+		list.add(p1);
+		list.add(p2);
+		list.add(new Person("Siddharth", 23));
+		list.add(new Person("Prashant", 23));
+		
+		//practice for loop now
+	}
+}
