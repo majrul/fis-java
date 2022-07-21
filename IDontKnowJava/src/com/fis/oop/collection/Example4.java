94c2cf2597856d698a7e051f1bcce0429f4c92c5
mj <mj@192.168.0.124>
2022-07-21 15:44:05 +0530

    java

--- IDontKnowJava/src/com/fis/oop/collection/Example4.java
+++ IDontKnowJava/src/com/fis/oop/collection/Example4.java
@@ -3,9 +3,12 @@ package com.fis.oop.collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.fis.oop.api.Person;
+
 public class Example4 {
 
 	public static void main(String[] args) {
+		//ip-address as the key, username as the value
 		Map<String, String> map = new HashMap<>();
 		map.put("192.168.1.1", "majrul");
 		map.put("192.168.1.2", "afaq");
@@ -13,5 +16,23 @@ public class Example4 {
 		
 		String user = map.get("192.168.1.1");
 		System.out.println(user);
+		
+		for(Map.Entry<String, String> entry : map.entrySet()) {
+			String ip = entry.getKey();
+			String usr = entry.getValue();
+			System.out.println(ip + " : " + usr);
+		}
+		
+		//aadharcard no as the key, person details as the value
+		Map<Integer, Person> map2 = new HashMap<>();
+		map2.put(1234567890, new Person("Majrul", 40));
+		map2.put(1122334455, new Person("Indrajeet", 22));
+		map2.put(1234512345, new Person("Mohit", 23));
+		map2.put(1321234545, new Person("Pragathi", 22));
+		
+		Person p = map2.get(1122334455);
+		System.out.println(p);
+		
+		
 	}
 }
