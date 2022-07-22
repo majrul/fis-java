ebaa968ca8479dd7a9e9db751efec7ce5cc4a0c1
mj <mj@localhost>
2022-07-22 17:30:32 +0530

    java

--- IDontKnowJava/src/com/fis/jdbc/InsertData3.java
+++ IDontKnowJava/src/com/fis/jdbc/InsertData3.java
@@ -2,19 +2,19 @@ package com.fis.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Scanner;
 
-public class InsertData2 {
+public class InsertData3 {
 
 	public static void main(String[] args) {
 		Connection conn = null;
-		Statement st = null;
+		PreparedStatement st = null;
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/training", "root", "passw0rd");
-			st = conn.createStatement();
+			st = conn.prepareStatement("insert into tbl_product(name, price, quantity) values(?, ?, ?)");
 			
 			Scanner s = new Scanner(System.in);
 			System.out.println("Enter product name");
@@ -23,10 +23,13 @@ public class InsertData2 {
 			double price = Double.parseDouble(s.nextLine());
 			System.out.println("Enter quantity");
 			int quantity = Integer.parseInt(s.nextLine());
+
+			//now substituting ? with the actual data
+			st.setString(1, name);
+			st.setDouble(2, price);
+			st.setInt(3, quantity);
+			st.executeUpdate();
 			
-			String sql = "insert into tbl_product(name, price, quantity) values('" + name + "'," + price + "," + quantity + ")";
-			st.executeUpdate(sql);
-			//st.executeUpdate("insert into tbl_product(name, price, quantity) values('iPhone 13 Pro', 120000, 75)"); //DML
 			System.out.println("Record inserted..");
 		}
 		catch(ClassNotFoundException | SQLException e) {
